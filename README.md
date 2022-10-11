2609c58f2ca896478b08ed7e800d1e6207ba8c53
huutam1991 <huutam1991@gmail.com>
2022-10-11 13:26:26 +0700

    main: add example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # json_cpp
 Very simple, very fast Json for C++
+
+# Usage
+Very simple, just copy files from 2 folder include/json + src/json to your project, then include <json/json.h> to the file's header
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <filesystem>
 
 #include <json/json.h>
 
 int main(int argc, char **argv)
 {
-    Json a = {
+    // Create example
+    Json json = {
         {"currency", "USDT"},
-        {"value", 24.55}
+        {"value", 24.55},
+        {
+            "user_info", {
+                {"user_name", "TamNguyen"},
+                {"team_size", 1},
+                {"is_developer", true},
+                {"is_devops", false}
+            }
+        }
     };
+
+    // Print out example
+    std::cout << "json = " << json << std::endl;
 
     std::cout << "Main exit" << std::endl;
 
